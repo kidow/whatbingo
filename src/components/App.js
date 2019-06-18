fd1952ba43e07318626ae538eed39ea12eb5e786
kidow <wcgo2ling@hanmail.net>
2019-06-18 14:48:34 +0900

    PageTemplate basic set

---
+++ src/components/App.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { PageTemplate } from 'components/templates'
+
+const App = () => {
+  return <PageTemplate>h1</PageTemplate>
+}
+
+export default App
---
+++ src/components/templates/PageTemplate/PageTemplate.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import './PageTemplate.css'
+
+const PageTemplate = ({ children }) => {
+  return <div className="page-template">{children}</div>
+}
+
+export default PageTemplate
