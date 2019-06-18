7e486535ba68cb29b6bd0b06e0d1d2f4a1a3d731
kidow <wcgo2ling@hanmail.net>
2019-06-18 16:04:47 +0900

    store basic set

--- src/index.js
+++ src/index.js
@@ -4,5 +4,21 @@ import 'lib/styles/index.scss'
 import App from './components/App'
 import * as serviceWorker from './serviceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import reducer from 'store'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import penderMiddleware from 'redux-pender'
+
+const store = createStore(
+  reducer,
+  composeWithDevTools(applyMiddleware(penderMiddleware()))
+)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
 serviceWorker.unregister()
